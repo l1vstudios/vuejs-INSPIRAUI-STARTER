9f3043147074d9255ff57fc50e12ddd672baaf80
Pantek <pantekjaya65@gmail.com>
2025-08-28 15:46:41 +0700

    Push

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,12 +9,10 @@ const menuOpen = ref(false)
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex items-center justify-between h-16">
         
-        <!-- Logo -->
         <div class="flex-shrink-0 text-2xl font-bold">
           MyApp
         </div>
 
-        <!-- Menu (Desktop) -->
         <div class="hidden md:flex space-x-6">
           <a href="#" class="hover:text-gray-300">Home</a>
           <a href="#" class="hover:text-gray-300">About</a>
@@ -22,7 +20,6 @@ const menuOpen = ref(false)
           <a href="#" class="hover:text-gray-300">Contact</a>
         </div>
 
-        <!-- Mobile Button -->
         <div class="md:hidden">
           <button @click="menuOpen = !menuOpen" class="focus:outline-none">
             <svg v-if="!menuOpen" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
@@ -40,7 +37,6 @@ const menuOpen = ref(false)
       </div>
     </div>
 
-    <!-- Mobile Menu -->
     <div v-if="menuOpen" class="md:hidden px-4 pb-3 space-y-2 bg-black/90">
       <a href="#" class="block py-2 hover:text-gray-300">Home</a>
       <a href="#" class="block py-2 hover:text-gray-300">About</a>
